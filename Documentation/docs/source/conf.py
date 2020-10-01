d79d5557ea5c5ba42c37f2923a56ab08f11aaa33
Carmen Perena <cperena@uw.edu>
2020-09-30 22:23:50 -0700

    Update conf.py

--- Documentation/docs/source/conf.py
+++ Documentation/docs/source/conf.py
@@ -12,10 +12,12 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
-sys.path.insert(0, os.path.abspath('../PyRKViewer'))
-sys.path.insert(0, os.path.abspath('../PyRKViewer/plugins'))
-sys.path.insert(0, os.path.abspath('../PyRKViewer/rkplugin'))
+sys.path.insert(0, os.path.abspath('https://github.com/evilnose/PyRKViewer'))
+sys.path.insert(0, os.path.abspath("https://github.com/evilnose/PyRKViewer"))
+sys.path.insert(0, os.path.abspath('https://github.com/evilnose/PyRKViewer/plugins'))
+sys.path.insert(0, os.path.abspath('https://github.com/evilnose/PyRKViewer/rkplugin'))
+sys.path.insert(0, os.path.abspath("https://github.com/evilnose/PyRKViewer/plugins"))
+sys.path.insert(0, os.path.abspath("https://github.com/evilnose/PyRKViewer/rkplugin"))
 
 
 # -- Project information -----------------------------------------------------

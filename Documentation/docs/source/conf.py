0017801eddd607bed30b7d300d4d6ebdfa98ba39
Carmen Perena <cperena@uw.edu>
2020-09-30 22:25:51 -0700

    Update conf.py

--- Documentation/docs/source/conf.py
+++ Documentation/docs/source/conf.py
@@ -18,6 +18,7 @@ sys.path.insert(0, os.path.abspath('https://github.com/evilnose/PyRKViewer/plugi
 sys.path.insert(0, os.path.abspath('https://github.com/evilnose/PyRKViewer/rkplugin'))
 sys.path.insert(0, os.path.abspath("https://github.com/evilnose/PyRKViewer/plugins"))
 sys.path.insert(0, os.path.abspath("https://github.com/evilnose/PyRKViewer/rkplugin"))
+sys.path.insert(0, os.path.abspath('../src'))
 
 
 # -- Project information -----------------------------------------------------

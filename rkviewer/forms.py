833411ba65e4cd1a58facfe00d06295a8cedfe95
Jin <jin.xu.2010@gmail.com>
2020-12-15 11:31:08 -0800

    right_click_align

--- rkviewer/forms.py
+++ rkviewer/forms.py
@@ -814,7 +814,7 @@ class NodeForm(EditPanelForm):
             fill, fill_alpha = self._GetMultiColor(list(n.fill_color for n in nodes))
             border, border_alpha = self._GetMultiColor(list(n.border_color for n in nodes))
             floatingNode = True
-            lockNode = True
+            lockNode = False
 
         self.pos_ctrl.Enable(self.contiguous)
         self.size_ctrl.Enable(self.contiguous)

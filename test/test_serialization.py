784119bf33447d4e9a4c23886cb6fbafd58d850d
nnguyen19 <minhnhannguyendo@gmail.com>
2021-06-05 23:07:14 -0500

    create all test cases for reactions and compartments

--- test/test_serialization.py
+++ test/test_serialization.py
@@ -1,15 +1,10 @@
 # pylint: disable=maybe-no-member
-from numpy.core.fromnumeric import shape
-from numpy.lib.polynomial import poly
 from test.api.common import DummyAppTest
-from typing import List
-from rkviewer.canvas.data import Reaction
-from rkviewer.mvc import CompartmentIndexError, NetIndexError, NodeIndexError, ReactionIndexError
 from rkviewer import iodine
-from rkviewer.plugin.api import Node, NodeData, Vec2
+from rkviewer.plugin.api import Color
+from rkviewer.config import Color
 import unittest
-import wx
-import time
+import numpy as np
 
 def createNode(nodeID, shapei= 0):
     #netID, nodeID, x, y, w, h, floatingNode, nodeLocked
@@ -22,24 +17,36 @@ def createAlias(nodeID, shapei = 0):
     iodine.addAliasNode(0, nodei, 0.3, 0.4, 3, 4)
     return nodei
 
-def createReaction():
-    reactanti = createNode("reactant", 0)
-    producti = createNode("product", 0)
-    # reactant = iodine.TNode(0, "reactant", Vec2(0.3,0.4), Vec2(3,4), floating= True, 
-    # nodeLocked= False)
-    # product = iodine.TNode(1, "product", Vec2(0.6,0.8), Vec2(6,8), floating= True, 
-    # nodeLocked= False)
-    # rxn = iodine.TReaction("rxn1", reactants={0: iodine.TSpeciesNode(1, reactant.position)}, products= {1: iodine.TSpeciesNode(1, product.position)})
-    # network = iodine.TNetwork("net1", {0:reactant,1:product}, reactions={0:rxn})
-    # network.addReaction(rxn)
-    iodine.createReaction(0, "rxn1", [reactanti], [producti])
+def createReaction(reactants = list([["reactant", 0]]), products = list([["product", 0]]), 
+                    sr_stoich = list([1]), dest_stoich = list([1])):
+    """stoich lists of # species in reactants/products """
+    source = []
+    dest = []
+    for i in range(len(reactants)):
+        reactanti = createNode(*reactants[i])
+        source.append(reactanti)
 
-def createCompartment():
-    iodine.addCompartment(0, "comp0", 0, 0, 100, 100)
-    iodine.addCompartment(0, "comp1", 0, 0, 100, 100)
-    node = createNode("node0")
-    #move node to compartment
-    iodine.setCompartmentOfNode(0, 0, 0)
+    for i in range(len(products)):
+        producti = createNode(*products[i])
+        dest.append(producti)
+        
+
+    iodine.createReaction(0, "test_rxn", source , dest)
+
+    for stoich in range(len(sr_stoich)):
+        iodine.setReactionSrcNodeStoich(0,0, source[stoich], sr_stoich[stoich])
+
+    for stoich in range(len(dest_stoich)):
+        iodine.setReactionDestNodeStoich(0,0, dest[stoich], dest_stoich[stoich])
+
+def createCompartment(compids = [["comp0",[]]]):
+    for comp in compids:
+        compi = iodine.addCompartment(0, comp[0], 0, 0, 100, 100)
+        for node in comp[1]:
+            #add node
+            nodei = createNode(*node)
+            #move node to compartment
+            iodine.setCompartmentOfNode(0, nodei, compi)
 
     #add following reaction
 
@@ -75,15 +82,6 @@ class TestSerialization(DummyAppTest):
         shape_items = shapeDict["items"][0]
         
         match_rectangle_primitive(self, shape_item = shape_items)
-        # self.assertEqual(3, len(shape_items[0]["border_color"]))
-        # self.assertIsInstance(shape_items[0]["border_width"], float)
-        # self.assertEqual("rectangle", shape_items[0]["name"])
-        # self.assertIsInstance(shape_items[0]["corner_radius"], float)
-        # self.assertEqual(4, len(shape_items[0]["fill_color"]))
-
-        # self.assertIsInstance(shape_items[1]["rotation"], float)
-        # self.assertEqual(2, len(shape_items[1]["scale"]))
-        # self.assertEqual(2, len(shape_items[1]["translation"]))
         
     def testTextPrimitive(self):
         node = createNode("node1")
@@ -170,27 +168,147 @@ class TestSerialization(DummyAppTest):
         self.assertEqual(0, anodeObj["originalIdx"])
         self.assertEqual(2, len(anodeObj["position"]))
         self.assertEqual(2, len(anodeObj["rectSize"]))
-    
-    def testCompartment(self):
-        #compartment with no node
-        #a node in compartment
-        # compartment with reaction
-        pass
 
     def testReaction(self):
         rxn = createReaction()
         dump_object = iodine.dumpNetwork(0)
 
         rxnDict = dump_object["reactions"]
-        #print(rxnDict)
+        rxnObj = rxnDict[0]
         self.assertEqual(1, len(rxnDict))
 
-        #a node as a reactant and product: 0 as reactant, 0 and 1 are products
-        #multiple reactants and products
+        self.assertIsInstance(rxnObj["bezierCurves"], bool)
+        self.assertEqual(2, len(rxnObj["centerHandlePos"]))
+        self.assertEqual(None, rxnObj["centerPos"])
+        self.assertEqual(3, len(rxnObj["fillColor"]))
+        self.assertIsInstance(rxnObj["id"], str)
+        self.assertIsInstance(rxnObj["modifiers"], list)
+        self.assertIsInstance(rxnObj["rateLaw"], str)
+        self.assertIsInstance(rxnObj["thickness"], float)
+        self.assertEqual(rxnObj["tipStyle"], "circle")
+
+    def testRxnCase1(self):
+        """
+        This test case against two seperate multiple reactant and product nodes
+        """
+        num_reactants = 11
+        sr_stoich = np.random.randint(1, 6, size = num_reactants)
+        reactant_ids = list(map(lambda i: 'sr'+str(i), np.arange(num_reactants)))
+        reactants = list(zip(reactant_ids, sr_stoich))
+
+        num_products = 13
+        dest_stoich = np.random.randint(1, 6, size = num_products)
+        product_ids = list(map(lambda i: 'dest'+str(i), np.arange(num_products)))
+        products = list(zip(product_ids, dest_stoich))
+
+        rxn = createReaction(reactants, products, sr_stoich = sr_stoich, dest_stoich = dest_stoich)
+        
+        dump_object = iodine.dumpNetwork(0)
+        rxnDict = dump_object["reactions"]
+        rxnObj = rxnDict[0]
+
+        reactantObj = rxnObj["reactants"]
+        productObj = rxnObj["products"]
+
+        self.assertEqual(len(sr_stoich), len(reactantObj))
+        self.assertEqual(len(dest_stoich), len(productObj))
+        
+        for i in reactantObj.keys():
+            match_rxn_node(self, reactantObj[i], sr_stoich[i])
+        
+        for i in productObj.keys():
+            match_rxn_node(self, productObj[i], dest_stoich[i-len(sr_stoich)])
+    
+    def testRxnCase2(self):
+        """
+        This test case is to examine a reaction with species existing in both reactant and product sides
+        """
+        sp_1 = ["e1", 0]
+        sp_2 = ["e2", 0]
+
+        node_1 = createNode(*sp_1)
+        node_2 = createNode(*sp_2)
+        
+        iodine.createReaction(0, "test_rxn", [node_1] , [node_1, node_2])
+        
+        dump_object = iodine.dumpNetwork(0)
+        rxnDict = dump_object["reactions"]
+        rxnObj = rxnDict[0]
+
+        reactantObj = rxnObj["reactants"]
+        productObj = rxnObj["products"]
+
+        self.assertEqual(1, len(reactantObj))
+        self.assertEqual(2, len(productObj))
+
+        for i in reactantObj.keys():
+            match_rxn_node(self, reactantObj[i], 1.)
+        
+        for i in productObj.keys():
+            match_rxn_node(self, productObj[i], 1.)
+
+        #Checking if node 1 shows up in both reactant and product sides
+        self.assertDictEqual(reactantObj[0], productObj[0])
+
+    def testEmptyCompartment(self):
+        comp = createCompartment()
+        dump_object = iodine.dumpNetwork(0)
+        compDict = dump_object["compartments"]
+        self.assertEqual(1, len(compDict))
+        compObj = compDict[0]
+        
+        self.assertEqual(3, len(compObj["fillColor"]))
+        self.assertEqual("comp0", compObj["id"])
+        self.assertEqual(3, len(compObj["outlineColor"]))
+        self.assertIsInstance(compObj["outlineThickness"], float)
+        self.assertEqual(2, len(compObj["position"]))
+        self.assertEqual(2, len(compObj["rectSize"]))
+        self.assertIsInstance(compObj["volume"], float)
+
+    def testCompartmentWithNode(self):
+        comp = createCompartment([["comp0", [["node0", 0], ["node1",2]]]])
+        dump_object = iodine.dumpNetwork(0)
+        compDict = dump_object["compartments"]
+        self.assertEqual(1, len(compDict))
+        compObj = compDict[0]
+        #check if nodes are in comp
+        nodeDict = dump_object["nodes"]
+        self.assertEqual(2, len(nodeDict))
+        self.assertEqual(0, nodeDict[0]['compi'])
+        self.assertEqual(0, nodeDict[1]['compi'])
+    
+    def testCompartmentWithReaction(self):
+        node_0 = ["node0", 0]
+        node_1 = ["node1",2]
+        comp = createCompartment([["comp0", [node_0, node_1]]])
+        #rxn = createReaction(reactants = [node_0], products = [node_1])
+        iodine.createReaction(0, "test_rxn", [0] , [1])
+        dump_object = iodine.dumpNetwork(0)
+
+        #check if nodes are in comp
+        nodeDict = dump_object["nodes"]
+        self.assertEqual(2, len(nodeDict))
+        self.assertEqual(0, nodeDict[0]['compi'])
+        self.assertEqual(0, nodeDict[1]['compi'])
+
+        #check reaction:
+        rxnDict = dump_object["reactions"]
+        self.assertEqual(1, len(rxnDict))
+        rxnObj = rxnDict[0]
+        self.assertEqual(11, len(rxnObj))
+
+    def testColorCompartment(self):
+        comp = createCompartment()
+        iodine.setCompartmentFillColor(0,0, color = Color(0, 0, 0, 100))
+        iodine.setCompartmentOutlineColor(0, 0, color = Color(0, 0, 0, 100))
+        dump_object = iodine.dumpNetwork(0)
+        compDict = dump_object["compartments"]
+        compObj = compDict[0]
+        self.assertEqual(4, len(compObj["outlineColor"]))
+        self.assertEqual(4, len(compObj["fillColor"]))
 
     def tearDown(self):
         iodine.clearNetworks()
-
 
 
 poly_shapes = {
@@ -236,10 +354,14 @@ def match_polygon_primitive(test_obj, shape_item, shape_name):
     test_obj.assertIsInstance(shape_item[0]["border_width"], float)
     test_obj.assertEqual(4, len(shape_item[0]["fill_color"]))
     test_obj.assertIsInstance(shape_item[0]["radius"], float)
-    test_obj.assertEqual(polygon_shapes[shape_name], len(shape_item[0]["points"]))
+    test_obj.assertEqual(poly_shapes[shape_name], len(shape_item[0]["points"]))
     [test_obj.assertEqual(2, len(point)) for point in shape_item[0]["points"]]
     
     #test transformation
     test_obj.assertIsInstance(shape_item[1]["rotation"], float)
     test_obj.assertEqual(2, len(shape_item[1]["scale"]))
     test_obj.assertEqual(2, len(shape_item[1]["translation"]))
+
+def match_rxn_node(test_obj, node, stoich):
+    test_obj.assertEqual(2, len(node["handlePos"]))
+    test_obj.assertEqual(stoich, node["stoich"])

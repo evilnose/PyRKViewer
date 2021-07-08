358ff851296d28b641470a3f6b181f046325b58a
samuels342 <csamuels@uw.edu>
2021-07-08 11:12:33 -0700

    Update pyproject.toml, update poetry.lock, rebuild requirements*.txt

--- scripts/gen_requirements.py
+++ scripts/gen_requirements.py
@@ -4,6 +4,6 @@ root_dir = os.path.join(os.path.realpath(__file__), '..', '..')
 def apath(filename):
     return os.path.abspath(os.path.join(root_dir, filename))
 
-os.system('poetry export -f requirements.txt --output {}'.format(apath('requirements.txt')))
-os.system('poetry export --dev -f requirements.txt --output {}'.format(apath('requirements-dev.txt')))
-os.system('poetry export -E sbml -f requirements.txt --output {}'.format(apath('requirements-sbml.txt')))
+os.system('poetry export -f requirements.txt --output "{}"'.format(apath('requirements.txt')))
+os.system('poetry export --dev -f requirements.txt --output "{}"'.format(apath('requirements-dev.txt')))
+os.system('poetry export -E sbml -f requirements.txt --output "{}"'.format(apath('requirements-sbml.txt')))

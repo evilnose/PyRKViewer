5825f6966140454e70b8c8cd0899a43d5762040a
Jin <jin.xu.2010@gmail.com>
2024-02-13 15:11:01 -0800

    version

--- rkviewer/view.py
+++ rkviewer/view.py
@@ -705,7 +705,7 @@ class MainFrame(wx.Frame):
     def onAboutDlg(self, event):
         info = wx.adv.AboutDialogInfo()
         info.SetName("SBcoyote")
-        info.SetVersion("1.5.0")
+        info.SetVersion("1.5.1")
         info.SetCopyright("(c) 2023 UW Sauro Lab")
         info.SetDescription("An Extensible Python-Based Reaction Editor and Viewer.")
         info.SetWebSite("https://github.com/sys-bio/SBcoyote",
